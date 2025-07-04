9decfa8b487f1b115382abed22790fc55c5f6557
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-07-04 21:12:06 +0300

    fix max screen size bugs

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import { HomeIcon, HashtagIcon, BellIcon, InboxIcon, BookmarkIcon, UserIcon, Ell
 
 const Sidebar = () => {
   return (
-    <div className="hidden sm:flex flex-col items-start p-4 space-y-4 w-64 h-screen border-r border-gray-200 fixed ml-42">
+    <div className="hidden sm:flex flex-col items-start p-4 space-y-4 w-64 h-screen border-r border-gray-200 fixed ml-74">
       {/* Menu Elements */}
       <button className="flex items-center space-x-3 hover:text-blue-500 p-2 rounded-full w-full cursor-pointer transition-all ease-in">
         <HomeIcon className="w-6 h-6"/>
@@ -39,7 +39,7 @@ const Sidebar = () => {
         <span>More</span>
       </button>
 
-      <button className="p-3 bg-blue-500 hover:bg-blue-600 text-white rounded-full w-full cursor-pointer transition-all ease-in relative">
+      <button className="p-3 bg-blue-600 hover:bg-blue-500 text-white rounded-full w-full cursor-pointer transition-all ease-in relative">
         Create a post  <SparklesIcon className="w-5 h-5 absolute left-10 top-3" />
       </button>
     </div>
