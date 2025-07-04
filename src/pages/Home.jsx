c2789e144dba059869ec518b92a9104aa225f59c
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-07-04 21:12:52 +0300

    import TweetBox component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar"
 import Sidebar from "../components/Sidebar"
+import TweetBox from "../components/TweetBox"
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Sidebar />
+      <TweetBox />
     </div>
   )
 }
