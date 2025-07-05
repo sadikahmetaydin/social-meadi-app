f483ecbb588a8ff3f304883e055ca3549ff76fec
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-07-05 14:23:41 +0300

    change application screen size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,31 @@
 import Navbar from "../components/Navbar"
 import Sidebar from "../components/Sidebar"
 import TweetBox from "../components/TweetBox"
+import Widgets from "../components/Widgets"
 
 const Home = () => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
-      <TweetBox />
+      <div className="min-h-screen">
+        <Navbar />
+
+        <div className="flex max-w-screen-xl mx-auto">
+
+          <div className="hidden md:block w-64">
+            <Sidebar />
+          </div>
+
+
+          <div className="flex-1">
+            <TweetBox />
+          </div>
+
+          <div className="hidden xl:block w-90">
+            <Widgets />
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
