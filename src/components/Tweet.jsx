9982dd45d7ff0f964b9b49b8f03701d8ee4f4b6f
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-07-08 20:23:29 +0300

    create TweetCart and import HomePage

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,6 +1,32 @@
-const Tweet = () => {
+import { ChatBubbleLeftEllipsisIcon, ArrowPathIcon, HandThumbUpIcon, ShareIcon } from "@heroicons/react/24/outline"
+
+const Tweet = ({name, username, avatar, content, time}) => {
   return (
-    <div>Tweet</div>
+    <div className="border-b border-gray-200 p-4 hover:bg-gray-100 transition-colors duration-200">
+      <div className="flex items-start space-x-4">
+
+        <img src={avatar} alt="Profile" className="w-12 h-12 rounded-full object-cover" />
+        
+        <div className="flex-1">
+          <div className="flex items-center justify-between">
+            <div>
+              <span className="text-sm font-bold">{name}</span>
+              <span className="text-gray-400 cursor-pointer ml-1 text-sm">@{username} . {time}</span>
+            </div>
+            <div className="text-gray-400 cursor-pointer hover:text-gray-600">•••</div>
+          </div>
+          
+          <p className="text-gray-600 mt-2 text-sm">{content}</p>
+
+          <div className="flex justify-between mt-5 text-gray-400 text-md">
+            <button className="hover:text-blue-400 transition-all ease-in"><ChatBubbleLeftEllipsisIcon className="w-6 h-6" /></button>
+            <button className="hover:text-blue-400 transition-all ease-in"><ArrowPathIcon className="w-6 h-6" /></button>
+            <button className="hover:text-blue-400 transition-all ease-in"><HandThumbUpIcon className="w-6 h-6" /></button>
+            <button className="hover:text-blue-400 transition-all ease-in"><ShareIcon className="w-6 h-6" /></button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Tweet
