fa47bc2cf32335fdff5036032fc38668db5be305
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-07-09 21:33:47 +0300

    install react router dom and structure inside App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage"
 import Home from "./pages/Home"
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
 
 const App = () => {
   return (
-    <div>
-      {/* <Home /> */}
-      <AuthPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/auth" element={ <AuthPage />} />
+        <Route path="/login" element={ <Login />} />
+        <Route path="/register" element={ <Register />} />
+      </Routes>
+    </Router>
   )
 }
 export default App
