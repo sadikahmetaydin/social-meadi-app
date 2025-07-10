4f0b2209e868b6b64b7ae2148de12eeb94498584
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-07-10 21:46:24 +0300

    install dotenv, hide Firebase apiKey and appId

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0JSJn6QiYt7-r7pjCzDvFk0l9cqln1Io",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "social-media-app-d1302.firebaseapp.com",
   projectId: "social-media-app-d1302",
   storageBucket: "social-media-app-d1302.firebasestorage.app",
   messagingSenderId: "234217506932",
-  appId: "1:234217506932:web:0fc3b2ecb5a7c37c866921"
+  appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
