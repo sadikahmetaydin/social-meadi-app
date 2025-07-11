a80713e01bafa5e71f21687244ac5d09f363d462
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-07-11 21:44:25 +0300

    create TweetBox functionality

--- src/components/TweetBox.jsx
+++ src/components/TweetBox.jsx
@@ -1,11 +1,15 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
+import { db } from "../firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useEffect, useState } from "react";
 import { SparklesIcon, PlusCircleIcon, PhotoIcon, GifIcon, ChartBarIcon, FaceSmileIcon, CalendarIcon, GlobeAltIcon } from "@heroicons/react/24/outline";
 
 export default function TweetBox() {
 
   const [user, setUser] = useState(null);
+  const [tweetText, setTweetText] = useState("");
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -14,6 +18,26 @@ export default function TweetBox() {
 
     return () => unsubscribe();
   }, []);
+
+  const sendTweet = async () => {
+    if (!tweetText.trim()) return;
+
+    try {
+      await addDoc(collection(db, "tweets"), {
+        text: tweetText,
+        createdAt: serverTimestamp(),
+        displayName: user.displayName,
+        photoURL: user.photoURL,
+        uid: user.uid
+      });
+
+      setTweetText("");
+      console.log("Tweet send successfully.");
+
+    } catch (error) {
+      console.log("Something went wrong! ", error.message);
+    }
+  }
 
 
   return (
@@ -31,7 +55,7 @@ export default function TweetBox() {
           }
 
           <div className="flex-1">
-            <textarea rows="3" placeholder="Share your idea..." className="w-full h-20 p-3 bg-gray-100 border-none outline-none placeholder-gray-400 rounded-md resize-none" />
+            <textarea value={tweetText} onChange={(e) => setTweetText(e.target.value)} rows="3" placeholder="Share your idea..." className="w-full h-20 p-3 bg-gray-100 border-none outline-none placeholder-gray-400 rounded-md resize-none" />
 
             <div className="flex items-center gap-1">
               <GlobeAltIcon className="text-blue-500 w-4 h-4" /> <span className="text-blue-500 text-sm"> Everyone can reply</span>
@@ -52,7 +76,7 @@ export default function TweetBox() {
                   <PlusCircleIcon className="w-7 h-7" />
                 </button>
 
-                <button className="bg-blue-600 hover:bg-blue-500 text-white p-3 px-10 rounded-full relative cursor-pointer">
+                <button onClick={sendTweet} className="bg-blue-600 hover:bg-blue-500 text-white p-3 px-10 rounded-full relative cursor-pointer">
                   Create a post <SparklesIcon className="w-5 h-5 absolute left-5 top-3" />
                 </button>
               </div>
