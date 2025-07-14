d2ad842ba2aa1ee1ef687761822c2f252526d73a
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-07-14 23:22:41 +0300

    show post list functionality in the HomePage

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,52 +1,61 @@
-import { onAuthStateChanged } from "firebase/auth"
-import { auth } from "../firebase"
+import { db } from "../firebase"
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore"
 import { useEffect, useState } from "react"
 import { ChatBubbleLeftEllipsisIcon, ArrowPathIcon, HandThumbUpIcon, ShareIcon, XCircleIcon } from "@heroicons/react/24/outline"
 
-const Tweet = ({name, username, avatar, content, time}) => {
+const Tweet = () => {
 
-  const [user, setUser] = useState(null);
+  const [tweets, setTweets] = useState([]);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    }, [])
+    const q = query(collection(db, "tweets"), orderBy("createdAt", "desc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id, ...doc.data()
+      }));
+      setTweets(data);
+    });
 
     return () => unsubscribe();
-  });
+  }, []);
 
   return (
-    <div className="border-b border-gray-200 p-4 hover:bg-gray-100 transition-colors duration-200">
-      <div className="flex items-start space-x-4">
-
-        {/* Update Photo with Firebase */}
-        {
-          user ? (
-            <img src={user.photoURL } alt="Profile" className="w-12 h-12 rounded-full object-cover cursor-pointer" />
-          ) : (
-            <img src={avatar} alt="Profile" className="w-12 h-12 rounded-full object-cover" />
-          )
-        }
-        
-        <div className="flex-1">
-          <div className="flex items-center justify-between">
-            <div>
-              <span className="text-sm font-bold">{name}</span>
-              <span className="text-gray-400 cursor-pointer ml-1 text-sm">@{username} . {time}</span>
+    <div className="space-y-4 mt-4">
+      {
+        tweets.map((tweet) => {
+          const { photoURL, displayName, createdAt, text, uid } = tweet;
+          const time = createdAt?.toDate().toLocaleString();
+          const username = uid?.slice(0, 6);
+    
+          return (
+            <div key={tweet.id} className="border-b border-gray-200 p-4 hover:bg-gray-100 transition-colors duration-200">
+              <div className="flex items-start space-x-4">
+    
+                <img src={photoURL } alt="Profile" className="w-12 h-12 rounded-full object-cover cursor-pointer" />
+                
+                <div className="flex-1">
+                  <div className="flex items-center justify-between">
+                    <div>
+                      <span className="text-sm font-bold">{displayName}</span>
+                      <span className="text-gray-400 cursor-pointer ml-1 text-sm">@{username} . {time}</span>
+                    </div>
+                    <div className="text-red-400 cursor-pointer hover:text-red-500"><XCircleIcon className="w-5 h-5" /></div>
+                  </div>
+                  
+                  <p className="text-gray-600 mt-2 text-sm">{text}</p>
+    
+                  <div className="flex justify-between mt-5 text-gray-400 text-md">
+                    <button className="hover:text-blue-400 transition-all ease-in"><ChatBubbleLeftEllipsisIcon className="w-6 h-6" /></button>
+                    <button className="hover:text-blue-400 transition-all ease-in"><ArrowPathIcon className="w-6 h-6" /></button>
+                    <button className="hover:text-blue-400 transition-all ease-in"><HandThumbUpIcon className="w-6 h-6" /></button>
+                    <button className="hover:text-blue-400 transition-all ease-in"><ShareIcon className="w-6 h-6" /></button>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="text-red-400 cursor-pointer hover:text-red-500"><XCircleIcon className="w-5 h-5" /></div>
-          </div>
-          
-          <p className="text-gray-600 mt-2 text-sm">{content}</p>
-
-          <div className="flex justify-between mt-5 text-gray-400 text-md">
-            <button className="hover:text-blue-400 transition-all ease-in"><ChatBubbleLeftEllipsisIcon className="w-6 h-6" /></button>
-            <button className="hover:text-blue-400 transition-all ease-in"><ArrowPathIcon className="w-6 h-6" /></button>
-            <button className="hover:text-blue-400 transition-all ease-in"><HandThumbUpIcon className="w-6 h-6" /></button>
-            <button className="hover:text-blue-400 transition-all ease-in"><ShareIcon className="w-6 h-6" /></button>
-          </div>
-        </div>
-      </div>
+          )
+        })
+      }
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,22 +20,7 @@ const Home = () => {
           <div className="flex-1">
             <TweetBox />
 
-            <Tweet 
-              name="Sadik Ahmet Aydin"
-              username="dev_sadik"
-              avatar="https://images.unsplash.com/photo-1511367461989-f85a21fda167?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dHdpdHRlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D"
-              content="The DesignLab is doing major work on Community-Driven Design, with several different variants. If you are working in this area, submit a paper to the DIS conference here in San Diego (see DesignLab posting below).... https://facebook.com/don.norman.18/..."
-              time="2h"
-            />
-
-            <Tweet 
-              name="Sadik Ahmet Aydin"
-              username="dev_sadik"
-              avatar="https://images.unsplash.com/photo-1511367461989-f85a21fda167?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dHdpdHRlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D"
-              content="YC W14's @NooraHealth launched an NFT auction to save thousands of lives. Read why @paulg bid here: http://paulgraham.com/nft.html"
-              time="2h"
-            />
-
+            <Tweet />
           </div>
 
           <div className="hidden xl:block w-90">
